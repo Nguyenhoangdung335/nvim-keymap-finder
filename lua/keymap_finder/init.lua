0893bee88d6118504aed3b95dda4baf430fea9d2
Nguyen Hoang Dung <nguyenhoangdung335@gmail.com>
2025-09-14 23:54:47 +0700

    update

--- lua/keymap_finder/init.lua
+++ lua/keymap_finder/init.lua
@@ -5,32 +5,31 @@ function M.get_keymaps()
         ["n"] = "Normal",
         ["i"] = "Insert",
         ["v"] = "Visual",
-        ["V"] = "V-Line",
+        ["x"] = "Visual",
         ["c"] = "Command",
         ["s"] = "Select",
-        ["S"] = "Select",
         ["t"] = "Terminal",
-        ["R"] = "Replace",
-        ["o"] = "Operator",
-        ["!"] = "Shell",
+        ["o"] = "Operator-pending",
     }
     local keymap_results = {}
 
-    for _, mode in ipairs(vim_modes) do
-        local keymaps = vim.api.nvim_get_keymap(mode:sub(1, 1))
+    for mode_char, mode_name in pairs(vim_modes) do
+        local keymaps = vim.api.nvim_get_keymap(mode_char)
         for _, keymap in ipairs(keymaps) do
-            table.insert(keymap_results, {
-                mode = mode,
-                lhs = keymap.lhs,
-                rhs = keymap.rhs,
-                desc = keymap.desc or "",
-                opts = {
-                    noremap = keymap.noremap,
-                    silent = keymap.silent,
-                    expr = keymap.expr,
-                    nowait = keymap.nowait,
-                },
-            })
+            if keymap.desc and keymap.desc ~= "" then
+                table.insert(keymap_results, {
+                    mode = mode_name,
+                    lhs = keymap.lhs,
+                    rhs = keymap.rhs,
+                    desc = keymap.desc or "",
+                    opts = {
+                        noremap = keymap.noremap,
+                        silent = keymap.silent,
+                        expr = keymap.expr,
+                        nowait = keymap.nowait,
+                    },
+                })
+            end
         end
     end
 
@@ -52,43 +51,48 @@ function M.pick_keymap()
 
     local keymaps = M.get_keymaps()
 
-    pickers.new(conf, {
-        prompt_title = "Pick a keymap",
-        finder = finders.new_table({
-            results = keymaps,
-            entry_maker = function(entry)
-                return {
-                    value = entry,
-                    display = string.format("[%s] %s -> %s %s", entry.mode, entry.lhs, entry.rhs, entry.desc),
-                    ordinal = entry.lhs .. " " .. entry.rhs .. " " .. entry.desc,
-                }
+    pickers
+        .new(conf, {
+            prompt_title = "Pick a keymap",
+            finder = finders.new_table({
+                results = keymaps,
+                entry_maker = function(entry)
+                    return {
+                        value = entry,
+                        display = string.format("[%s] %s -> %s %s", entry.mode, entry.lhs, entry.rhs, entry.desc),
+                        ordinal = entry.lhs .. " " .. entry.rhs .. " " .. entry.desc,
+                    }
+                end,
+            }),
+            sorter = conf.generic_sorter({}),
+            attach_mappings = function(prompt_bufnr, map)
+                -- This is your safe "show notification" action, which is great.
+                -- It will be the default action for <CR> in normal mode.
+                actions.select_default:replace(function()
+                    local selection = action_state.get_selected_entry()
+                    vim.notify(string.format("LHS: %s | RHS: %s", selection.value.lhs, selection.value.rhs))
+                end)
+
+                -- Add a more powerful mapping for <C-e> to "execute" the keymap
+                map("i", "<C-e>", function()
+                    local selection = action_state.get_selected_entry()
+                    actions.close(prompt_bufnr)
+                    -- Feeds the keys to Neovim as if you typed them
+                    vim.api.nvim_feedkeys(selection.value.lhs, "n", true)
+                    vim.notify(string.format("Executed '%s'", selection.value.lhs))
+                end)
+
+                -- Add a mapping to copy the keymap's left-hand-side to your clipboard
+                map("i", "<C-y>", function()
+                    local selection = action_state.get_selected_entry()
+                    vim.fn.setreg("+", selection.value.lhs)
+                    vim.notify(string.format("Yanked '%s' to clipboard", selection.value.lhs))
+                end)
+
+                return true
             end,
-        }),
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr, map)
-            -- actions.select_default:replace(function()
-            --     actions.close(prompt_bufnr)
-            --     local selection = action_state.get_selected_entry()
-            --     vim.api.nvim_feedkeys(selection.value.lhs, "n", true)
-            -- end)
-            -- return true
-            map("i", "<CR>", function()
-                local selection = ation_state.get_selected_entry()
-                actions.close(prompt_bufnr)
-                vim.notify(
-                    string.format(
-                        "Keymap: [%s] %s -> %s %s",
-                        selection.value.mode,
-                        selection.value.lhs,
-                        selection.value.rhs,
-                        selection.value.desc
-                    ),
-                    vim.log.levels.INFO
-                )
-            end)
-            return true
-        end,
-    }):find()
+        })
+        :find()
 end
 
 return M
